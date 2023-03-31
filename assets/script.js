92426549ffb9927763cd207666295f307ba5e087
Sean Hayes <hayessea000@outlook.com>
2023-03-31 16:11:27 -0500

    added error alerts

--- assets/script.js
+++ assets/script.js
@@ -20,7 +20,6 @@ var popup = function(){
   up = confirm("Do you want to include UPPERCASE LETTERS in you password? Click OK for yes or Cancel for no.")
   low = confirm("Do you want to include LOWERCASE LETTERS in you password? Click OK for yes or Cancel for no.")
   sy = confirm("Do you want to include SPECIAL CHARACTERS in you password? Click OK for yes or Cancel for no.")
-  make()
   long = prompt("How long do you want your password to be?\nMust be between 8 and 128.")
 }
 
@@ -37,90 +36,32 @@ var make =function(){
   }if (sy==true){
     all= all.concat(symbol)
     alert("SPECIAL CHARACTERS will be used in password.")
-  }if (num != true && up != true && low != true && sy != true){ 
-    console.log ("you need something")
   }
 }
 
 var error = function(){
   if (num != true && up != true && low != true && sy != true){
     alert("You MUST use at least 1 kind of character. Try again.")
-  }if (8<= long <= 128){
+  }if (long < 8 || long > 128){
     alert("The length Must be between 8 and 128 . Try again.")
   }
 }
 
 var random =function(){
-  er = Math.floor(Math.random() * /* change to all*/ number.length);
-
-  console.log(all) /* remove log*/
-  item = /* change to all*/number[er]
+  er = Math.floor(Math.random() *  all.length);
+  item = all[er]
   return item;
 }
 
-
+console.log()
 var generatePassword = function(){
-  console.log(number)
   popup()
+  make()
   random()
+  console.log(all)
+  error()
 return item
 }
-// if (num==true && up==true && low==true && sy==true){
-//     all = number.concat(upperCase,lowerCase,symbol)
-//     console.log (all)
-//     10
-// }else if (num==true && up==true && low==true && sy==false){
-//     all = number.concat(upperCase,lowerCase)
-//     console.log (all)
-// }else if (num==true && up==true && low==false && sy==true){
-//     all = number.concat(upperCase,symbol)
-//     console.log (all)
-// }else if (num==true && up==false && low==true && sy==true){
-//     all = number.concat(symbol,lowerCase)
-//     console.log (all)
-// }else if (num==false && up==true && low==true && sy==true){
-//     all = symbol.concat(upperCase,lowerCase)
-//     console.log (all)
-// }else if (num==true && up==true && low==false && sy==false){
-//     all = number.concat(upperCase)
-//     console.log (all)
-// }else if (num==true && up==false && low==true && sy==false){
-//     all = number.concat(lowerCase)
-//     console.log (all)
-// }else if (num==false && up==true && low==true && sy==false){
-//     all = upperCase.concat(lowerCase)
-//     console.log (all)
-// }else if (num==true && up==false && low==false && sy==true){
-//     all = number.concat(symbol)
-//     console.log (all)
-// }else if (num==false && up==true && low==false && sy==true){
-//     all = symbol.concat(upperCase)
-//     console.log (all)
-// }else if (num==false && up==false && low==true && sy==true){
-//     all = symbol.concat(lowerCase)
-//     console.log (all)
-// }else if (num==false && up==false && low==false && sy==true){
-//     all = symbol
-//     console.log (all)
-// }else if (num==false && up==false && low==true && sy==false){
-//     all = lowerCase
-//     console.log (all)
-// }else if (num==false && up==true && low==false && sy==false){
-//     all = upperCase
-//     console.log (all)
-// }else if (num==true && up==false && low==false && sy==false){
-//     all = number
-//     console.log (all)
-// }else if (num==false && up==false && low==false && sy==false){
-//     console.log ("you need something")
-// }else{
-//     console.log("error")
-// }
-
-
-// }
-// need create 
-
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
