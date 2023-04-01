d729305894d28e2b58e9c8f1475b960107c77890
Sean Hayes <hayessea000@outlook.com>
2023-03-31 19:55:11 -0500

    finishing touches to the code

--- assets/script.js
+++ assets/script.js
@@ -7,42 +7,40 @@ var numberPrompt
 var upperCasePrompt
 var lowerCasePrompt
 var symbolPrompt
-var length
+var passLength
 var generated
-var selected
 
 var popup = function(){
-  numberPrompt = confirm("Do you want to include NUMBERS in you password? Click OK for yes or Cancel for no.")
-  upperCasePrompt = confirm("Do you want to include UPPERCASE LETTERS in you password? Click OK for yes or Cancel for no.")
-  lowerCasePrompt = confirm("Do you want to include LOWERCASE LETTERS in you password? Click OK for yes or Cancel for no.")
-  symbolPrompt = confirm("Do you want to include SPECIAL CHARACTERS in you password? Click OK for yes or Cancel for no.")
-  length = prompt("How long do you want your password to be?\nMust be between 8 and 128.")
+  numberPrompt = confirm("Do you want to include NUMBERS in you password?\nClick OK for yes or Cancel for no.")
+  upperCasePrompt = confirm("Do you want to include UPPERCASE LETTERS in you password?\nClick OK for yes or Cancel for no.")
+  lowerCasePrompt = confirm("Do you want to include LOWERCASE LETTERS in you password?\nClick OK for yes or Cancel for no.")
+  symbolPrompt = confirm("Do you want to include SPECIAL CHARACTERS in you password?\nClick OK for yes or Cancel for no.")
+  passLength = prompt("How long do you want your password to be?\nMust be between 8 and 128.")
 }
 
 var make =function(){
   generated=[]
   all =[]
-  selected=[]
   if (numberPrompt==true){
     all = all.concat(number)
-     alert("NUMBERS will be used in password.")
+    alert("NUMBERS can be used in password.")
   }if (upperCasePrompt==true){
     all= all.concat(upperCase)
-     alert("UPPERCASE LETTERS will be used in password.")
+    alert("UPPERCASE LETTERS can be used in password.")
   }if (lowerCasePrompt==true){
     all= all.concat(lowerCase)
-     alert("LOWERCASE LETTERS will be used in password.")
+    alert("LOWERCASE LETTERS can be used in password.")
   }if (symbolPrompt==true){
     all= all.concat(symbol)
-     alert("SPECIAL CHARACTERS will be used in password.")
+    alert("SPECIAL CHARACTERS can be used in password.")
   }if (numberPrompt != true && upperCasePrompt != true && lowerCasePrompt != true && symbolPrompt != true){
     alert("You MUST use at least 1 kind of character. Try again.")
   }
 }
 
 var random =function(){
-  if (length >= 8 && length <= 128){
-    for( i = 0; i< length; i++){
+  if (passLength >= 8 && passLength <= 128){
+    for( i = 0; i< passLength; i++){
     var randomNumber = Math.floor(Math.random() *  all.length);
     generated.push(all[randomNumber])
     }
